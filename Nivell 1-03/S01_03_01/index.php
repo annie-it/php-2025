ae037e717c3e4476c8d4837843a74380a289ec46
Annie EK <annie.beratz@proton.me>
2025-11-17 14:04:18 +0100

    Add:Nivel-1-03

---
+++ Nivell 1-03/S01_03_01/index.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sprint1</title>
+</head>
+<body> 
+    <?php 
+    echo"<h2>Ejercicio 1</h2>";
+
+    $array = [1, 2, 3, 4, 5];
+
+    print_r($array); 
+    ?>
+
+</body>
+</html>
